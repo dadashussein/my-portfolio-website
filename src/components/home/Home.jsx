c0f07fc06d4d56c71e0e13a5aa8e19db3d2583f3
Dadash Hussein <huseynzade.dadas@gmail.com>
2022-12-25 14:30:16 +0400

    home section added

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ".//home.css";
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
+import Social from "./Social";
+
+const Home = () => {
+  return (
+    <div>
+      <section className="home section" id="home">
+        <div className="home__container container grid">
+          <div className="home__content grid">
+            <Social />
+
+            <div className="home__img"></div>
+              <Data />
+
+          </div>
+          <ScrollDown />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;

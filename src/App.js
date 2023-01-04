c53564af88113001866eace3995b254e27abc580
Dadash Hussein <huseynzade.dadas@gmail.com>
2023-01-04 11:51:56 +0400

    footer_add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Qualification from "./components/qualification/Qualification";
+import ScrollUp from "./components/scrollup/ScrollUp";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
 
@@ -19,6 +21,9 @@ function App() {
         <Qualification />
         <Contact />
       </main>
+
+      <Footer />
+      <ScrollUp />
     </>
   );
 }

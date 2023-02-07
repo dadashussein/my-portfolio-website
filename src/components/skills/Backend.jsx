7812ca5d76a17385c56303e316ef5721ffc585b3
Dadash Hussein <huseynzade.dadas@gmail.com>
2023-02-07 12:07:04 +0400

    upgrade

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,50 +3,40 @@ import React from "react";
 const Backend = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Backend Developer</h3>
-
-      <div className="skills__box">
-        <div className="skills__group">
-          <div className="skills__data">
-            <i class="bx bxl-php"></i>
-
-            <div>
-              <h3 className="skills__name">PHP</h3>
-              <span className="skills__level">Basic</span>
-            </div>
-          </div>
-
-          <div className="skills__data">
-            <i class="bx bxl-nodejs"></i>
-
-            <div>
-              <h3 className="skills__name">Node JS</h3>
-              <span className="skills__level">Basic</span>
-            </div>
-          </div>
+    <h3 className="skills__title">JavaScript</h3>
+    <div className="skills__box">
+      <div className="skills__group">
+        <div className="skills__data">
+        <i className='bx bxl-jquery'></i>
+          <h3 className="skills__name">jQuery</h3>
         </div>
-
-        <div className="skills__group">
-          <div className="skills__data">
-            <i class="bx bxl-python"></i>
-
-            <div>
-              <h3 className="skills__name">Python</h3>
-              <span className="skills__level">Basic</span>
-            </div>
-          </div>
-
-          <div className="skills__data">
-            <i class="bx bxs-data"></i>
-
-            <div>
-              <h3 className="skills__name">My SQL</h3>
-              <span className="skills__level">Basic</span>
-            </div>
+        <div className="skills__data">
+        <i className="bx bxl-react"></i>
+          <div>
+            <h3 className="skills__name">React</h3>
           </div>
         </div>
       </div>
+      <div className="skills__group">
+        <div className="skills__data">
+        <i class='bx bxl-redux'></i>
+          <h3 className="skills__name">Redux</h3>
+        </div>
+      </div>
+      <div className="skills__group">
+        <div className="skills__data">
+          <i className="bx bxl-react"></i>
+          <h3 className="skills__name">Next JS</h3>
+        </div>
+      </div>
+      <div className="skills__group">
+        <div className="skills__data">
+        <i className='bx bxl-typescript'></i>
+          <h3 className="skills__name">TypeScript</h3>
+        </div>
+      </div>
     </div>
+  </div>
   );
 };
 
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -3,45 +3,36 @@ import React from "react";
 const Frontend = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Frontend developer</h3>
-
+      <h3 className="skills__title">CSS </h3>
       <div className="skills__box">
         <div className="skills__group">
           <div className="skills__data">
-            <i class="bx bxl-html5"></i>
-            <div>
-              <h3 className="skills__name">HTML</h3>
-              <span className="skills__level">Basic</span>
-            </div>
+            <i class="bx bxl-bootstrap"></i>
+            <h3 className="skills__name">Bootstrap</h3>
           </div>
-
           <div className="skills__data">
-            <i class="bx bxl-css3"></i>
+            <i class="bx bxl-sass"></i>
             <div>
-              <h3 className="skills__name">CSS</h3>
-              <span className="skills__level">Advanced</span>
+              <h3 className="skills__name">Sass</h3>
             </div>
           </div>
         </div>
-
         <div className="skills__group">
           <div className="skills__data">
             <i class="bx bxl-tailwind-css"></i>
-
-            <div>
-              <h3 className="skills__name">Tailwind</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
+            <h3 className="skills__name">Tailwind</h3>
           </div>
         </div>
-
         <div className="skills__group">
           <div className="skills__data">
-            <i class="bx bxl-react"></i>
-            <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
+            <i class="bx bxl-css3"></i>
+            <h3 className="skills__name">Material UI</h3>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bxl-css3"></i>
+            <h3 className="skills__name">Styled</h3>
           </div>
         </div>
       </div>
